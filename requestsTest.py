0f072f40742cfa8ce3943da1006b64f7ae04af97
d-sato2 <https://d-sato2@github.com/d-sato2/Map_Rails>
2016-05-04 17:32:34 +0900

    スクレイピングrequetTest.pyに時刻編集機能追加

--- requestsTest.py
+++ requestsTest.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import requests
+from datetime import datetime
 
-url = "http://127.0.0.1:8000/show/9"
+url = "http://127.0.0.1:8000/show/4"
 response = requests.get(url)
 html = response.text.encode(response.encoding)
 
@@ -26,14 +27,28 @@ Lang = html[firstLang:lastLang]
 print Lang
 
 firstMemo = html.find("memo : ") + 7
-lastMemo = html.find("以上。") -3
+lastMemo = html.find("start time : ") -3
 Memo = html[firstMemo:lastMemo]
 print Memo
+
+firstTime = html.find("start time : ") + 13
+lastTime = html.find("以上。") -3
+StartTime = html[firstTime:lastTime]
+print StartTime
+inttime = datetime.strptime(StartTime, '%Y-%m-%d %H:%M:%S')
+print inttime
+print inttime.year
+print inttime.month
+print inttime.day
+print inttime.hour
+print inttime.minute
+print inttime.second
 
 content = "ID" + Id + "の"
 content += "QRコードは" + Qr
 content += "、名前は" + Name
 content += "、母国語は" + Lang
 content += "、メモは" + Memo
+content += "、出発時刻は" + StartTime
 content += "です。"
 print content
